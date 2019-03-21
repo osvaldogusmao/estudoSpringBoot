3d73ac3b8a80de6d70cce98c81e3e7db70331bd5
Osvaldo Gusmão <osvaldo.gusmao@outlook.com>
2019-03-21 01:00:10 -0300

    Criando classe de domínio : Categoria

--- src/main/java/br/com/osvaldogusmao/resources/CategoriaResource.java
+++ src/main/java/br/com/osvaldogusmao/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.osvaldogusmao.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.osvaldogusmao.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+
+		Categoria catUm = new Categoria(1, "Categoria Um");
+		Categoria catDois = new Categoria(2, "Categoria Dois");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(catUm);
+		lista.add(catDois);
+
+		return lista;
 	}
-	
+
 }
